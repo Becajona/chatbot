9ed1c442880470d5c617705b31ba9e06a87373e6
Jonathan <utp0155232@alumno.utpuebla.edu.mx>
2024-10-01 15:49:50 -0600

    first commit

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const path = require('path'); // Importa el módulo path
+
 
 const app = express();
 
 // Middleware para parsear JSON y configurar CORS
 app.use(express.json());
 app.use(cors());
+
+
+
+// Sirve archivos estáticos desde el directorio "public"
+app.use(express.static(path.join(__dirname, '../public')));
 
 
 const  API_KEY = 'AIzaSyAxj6XnMi3RAGmDvJvpJbc22IGZZo1DKIM'; // Reemplaza por tu API key
@@ -42,10 +49,12 @@ app.post('/chat', async (req, res) => {
         res.json({ response: messageText });
 
     } catch (error) {
+        console.error('Error from API:', error.response ? error.response.data : error.message);
         res.status(500).json({ error: 'Failed to get response from API' });
     }
+    
 });
-const PORT = process.env.PORT || 3000;  // Usa el puerto asignado por Railway o 3000 como fallback
+const PORT = process.env.PORT || 3300;  // Usa el puerto asignado por Railway o 3000 como fallback
 
 // Iniciar el servidor
 app.listen(PORT, () => {
